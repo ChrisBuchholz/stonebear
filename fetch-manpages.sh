b0e0ddade64871b8e38c9c373ad8c1cf003e12ca
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2011-09-01 23:24:53 +0200

    add manpages

---
+++ fetch-manpages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.github.com/ChrisBuchholz/stonebear/master/man/man1/stonebear.1.html -O man/man1/stonebear.1.html
+wget https://raw.github.com/ChrisBuchholz/stonebear/master/man/man2/stonebear-config.2.html -O man/man2/stonebear-config.2.html
