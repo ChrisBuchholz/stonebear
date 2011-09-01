d70cb30f57290dad071f7d76a64dc3e17da8e71a
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2011-09-01 12:51:02 +0200

    modify setup

--- setup.py
+++ setup.py
@@ -4,33 +4,30 @@ from distutils.core import setup
 import stonebear
 
 setup(
-        name = "stonebear",
-        version = stonebear.__version__,
-        description = "",
+        name =             'stonebear',
+        version =          stonebear.__version__,
+        description =      'Build and deployment tool for developing websites and web applications',
         long_description = open('README.rst').read(),
-        author = "chrisbuchholz",
-        author_email = "christoffer.buchholz@gmail.com",
-        license = "",
-        platforms = [
-            "MacOS",
-            "POSIX"
-        ],
-        url = "https://github.com/ChrisBuchholz/stonebear",
-        packages = ["stonebear"],
-        package_data = {},
-        requires = ["PyYAML (>=3.x)"],
-        scripts = ["bin/stonebear"],
-        data_files = [("man/man1", ["man/man1/stonebear.1"])],
-        classifiers = [
-            "Development Status :: 5 - Production/Stable",
-            "Environment :: Console",
-            "Intended Audience :: End Users/Desktop",
-            "Intended Audience :: Developers",
-            "Intended Audience :: System Administrators",
-            "Natural Language :: English",
-            "Operating System :: MacOS",
-            "Operating System :: POSIX",
-            "Programming Language :: Python",
-            "Topic :: Utilities"
+        author =           'chrisbuchholz',
+        author_email =     'christoffer.buchholz@gmail.com',
+        maintainer =       'chrisbuchholz',
+        maintainer_email = 'christoffer.buchholz@gmail.com',
+        license =          'GNU General Public License (GPL)',
+        platforms =        ['MacOS', 'POSIX'],
+        url =              'https://github.com/ChrisBuchholz/stonebear',
+        packages =         ['stonebear'],
+        package_data =     {},
+        requires =         [],
+        scripts =          ['bin/stonebear'],
+        data_files =       [('man/man1', ['man/man1/stonebear.1'])],
+        classifiers =      [
+            'Environment :: Console',
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers'
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Natural Language :: English',
+            'Operating System :: MacOS :: MacOS X'
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 2.7'
         ]
 )
