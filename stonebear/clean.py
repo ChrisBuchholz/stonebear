2c1d694a3ced81a0766429088dfad9edf3e6ad46
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2011-09-04 21:06:24 +0200

    clean up tests

--- stonebear/clean.py
+++ stonebear/clean.py
@@ -12,19 +12,23 @@ def clean(args, config):
     # run preclean command
     subprocess.call(config['preclean'], shell=True)
 
-    # clean output_dir
-    output_dir = os.getcwd() + '/' + config['output_dir']
-    try:
-        if os.path.exists(output_dir):
-            shutil.rmtree(output_dir)
-
-        print "clean output_dir"
-    except OSError as exc:
-        if os.path.isdir(output_dir):
-            print "could not modify %s" % output_dir
-        else:
-            raise
-        sys.exit(1)
+    # remove all files/folders from [output]
+    cwd = os.getcwd() + '/'
+    for o in config['output']:
+        co = cwd + o
+        if os.path.exists(co):
+            if os.path.isdir(co):
+                try:
+                    shutil.rmtree(co)
+                except OSError:
+                    raise
+                    sys.exit(1)
+            else:
+                try:
+                    os.remove(co)
+                except OSError:
+                    raise
+                    sys.exit(1)
 
     # run postclean command
     subprocess.call(config['postclean'], shell=True)
