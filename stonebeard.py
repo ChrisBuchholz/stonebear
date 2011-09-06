39acaf6a02be2ee15bc9f4a50e1d6692ac3521d9
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2011-09-06 15:04:45 +0200

    something

--- stonebeard.py
+++ stonebeard.py
@@ -1,8 +1,15 @@
 config = {
+    'input':    ['stonebear.1.html', 'stonebear-config.2.html'],
+    'output':   ['man/man1/stonebear.1.html',
+                 'man/man2/stonebear-config.2.html'],
     'prebuild': """
-        wget https://raw.github.com/ChrisBuchholz/stonebear/master/man/man1/stonebear.1.html -O man/man1/stonebear.1.html
-        wget https://raw.github.com/ChrisBuchholz/stonebear/master/man/man2/stonebear-config.2.html -O man/man2/stonebear-config.2.html
-
-        mkdir -p man man/man1 man/man2
+        wget https://raw.github.com/ChrisBuchholz/stonebear/master/man/man1/stonebear.1.html
+        wget https://raw.github.com/ChrisBuchholz/stonebear/master/man/man2/stonebear-config.2.html
+    """,
+    'postbuild': """
+        rm stonebear.1.html stonebear-config.2.html
+    """,
+    'postclean': """
+        rm -rf man/
     """
 }
